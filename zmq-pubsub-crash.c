f1c663603616e38e47ec215f160a2a59f7206aff
Heungsub Lee <sub@subl.ee>
2018-02-14 11:45:58 +0900

    c90 compatible

--- zmq-pubsub-crash.c
+++ zmq-pubsub-crash.c
@@ -7,6 +7,7 @@ int main()
 
     int hwm = 1;
     int n = 10000;
+    int i, j;
 
     char addr[256]; size_t addr_len = 256;
     char opt[256];  size_t opt_len  = 256;
@@ -18,7 +19,7 @@ int main()
     pub = zmq_socket(context, ZMQ_PUB);
     zmq_setsockopt(pub, ZMQ_RCVHWM, &hwm, sizeof(hwm));
 
-    for (int i = 0; i < 100; ++i)
+    for (i = 0; i < 100; ++i)
     {
         sub = zmq_socket(context, ZMQ_SUB);
         zmq_setsockopt(sub, ZMQ_SNDHWM, &hwm, sizeof(hwm));
@@ -26,16 +27,15 @@ int main()
         zmq_bind(sub, "tcp://127.0.0.1:*");
         zmq_getsockopt(sub, ZMQ_LAST_ENDPOINT, addr, &addr_len);
 
-        for (int j = 0; j < n; ++j)
+        for (j = 0; j < n; ++j)
         {
             sprintf(topic, "%08x", j);
             zmq_setsockopt(sub, ZMQ_SUBSCRIBE, &topic, 8);
         }
 
-        // zmq_connect(sub, addr);
         zmq_connect(pub, addr);
 
-        for (int j = 0; j < n; ++j)
+        for (j = 0; j < n; ++j)
         {
             sprintf(topic, "%08x", j);
             zmq_setsockopt(sub, ZMQ_UNSUBSCRIBE, &topic, 8);
